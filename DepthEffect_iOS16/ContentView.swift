2c0c4a7fdf1000a41a091c3d85093ecb085de63a
isaaclopez <exolivil@gmail.com>
2022-08-22 13:13:00 -0500

    Initial Commit

---
+++ DepthEffect_iOS16/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DepthEffect_iOS16
+//
+//  Created by Jose Isaac on 22/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DepthEffect_iOS16/DepthEffect_iOS16App.swift
@@ -0,0 +1,17 @@
+//
+//  DepthEffect_iOS16App.swift
+//  DepthEffect_iOS16
+//
+//  Created by Jose Isaac on 22/08/22.
+//
+
+import SwiftUI
+
+@main
+struct DepthEffect_iOS16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
